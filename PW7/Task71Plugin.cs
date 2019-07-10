dfdeb47d153d8af9085aebe1b8b118f91430e627
Kostia007 <31204303+Kostia007@users.noreply.github.com>
2019-07-10 12:04:43 +0300

    Add files via upload

---
+++ PW7/Task71Plugin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+
+namespace Task71Plugin
+{
+    public class Task71 : Plugin
+    {
+        public Task71()
+            : base(typeof(Task71))
+        {
+            base.RegisteredEvents
+                .Add(new Tuple<int, string, string, Action<LocalPluginContext>>((int)PluginStage.PreOperation, "Create",
+                    "new_testentity",
+                    PullThePlugin));
+        }
+
+        protected void PullThePlugin(LocalPluginContext localContext)
+        {
+            if (!localContext.PluginExecutionContext.InputParameters.Contains("Target"))
+            {
+                return;
+            }
+            Entity target = (Entity)localContext.PluginExecutionContext.InputParameters["Target"];
+            string textfield = target.GetAttributeValue<string>("new_testtextfield");
+            IOrganizationService service = localContext.OrganizationService;
+            Random rand = new Random();
+           target["new_testtextfield"] = rand.Next().ToString();
+        }
+    }
+}
---
+++ PW7/Task72Plugin.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+
+namespace Task72Plugin
+{
+    public class Task72 : Plugin
+    {
+        public Task72()
+            : base(typeof(Task72))
+        {
+            base.RegisteredEvents
+                .Add(new Tuple<int, string, string, Action<LocalPluginContext>>((int)PluginStage.PreOperation, "Update",
+                    "new_testentity",
+                    PullThePlugin));
+        }
+
+        protected void PullThePlugin(LocalPluginContext localContext)
+        {
+            if (!localContext.PluginExecutionContext.InputParameters.Contains("Target"))
+            {
+                return;
+            }
+            IOrganizationService service = localContext.OrganizationService;
+            Entity target = (Entity)localContext.PluginExecutionContext.InputParameters["Target"];
+            EntityReference lookupRef = target.GetAttributeValue<EntityReference>("new_lookup");
+            if (lookupRef != null)
+            {
+                Entity acc = service.Retrieve(lookupRef.LogicalName, lookupRef.Id, new Microsoft.Xrm.Sdk.Query.ColumnSet("emailaddress1"));
+                string email = acc.GetAttributeValue<string>("emailaddress1");
+                if (email == null)
+                {
+                    target["new_textforemail"] = "";
+                    localContext.Trace("Trace error Trace error Trace error Trace error");
+                    throw new InvalidPluginExecutionException("Client`s email is empty!");
+                }
+                else
+                {
+                    ITracingService tracingService = localContext.TracingService;
+                    tracingService.Trace("Trace success Trace success Trace success Trace success");
+                    target["new_textforemail"] = "Customer meets the requirements.";
+                }
+            }
+
+
+        }
+    }
+}
